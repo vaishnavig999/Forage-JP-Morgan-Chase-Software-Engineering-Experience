489cd097c1dd928fcf53b7c2fc1a8d238afd8b30
Vaishnavi <vaishnavigaikwad3939@gmail.com>
2024-07-17 16:03:10 +0530

    Squashed 'task_1-folder/' content from commit df57886
    git-subtree-dir: task_1-folder
    git-subtree-split: df57886abe23d02953b0123d951bfcbaa3b18e4f

---
+++ client_test.py
@@ -0,0 +1,33 @@
+import unittest
+from client3 import getDataPoint
+from client3 import getRatio
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_ask']['price'] + quote['top_bid']['price']) / 2))
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 120.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_ask']['price'] + quote['top_bid']['price']) / 2))
+
+
+  """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self):
+    self.assertEqual(getRatio(120.2, 120.68), 120.2 / 120.68)
+    self.assertEqual(getRatio(121.2, 0), None) 
+
+if __name__ == '__main__':
+    unittest.main()
